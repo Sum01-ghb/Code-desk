013ab79a3c06928c09f9cf32df267f0c18a78733
Sumit <sumit500241@gmail.com>
2025-12-29 18:31:35 +0530

    Setup done

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
+      <Button>Get started</Button>
+    </div>
+  );
+}
